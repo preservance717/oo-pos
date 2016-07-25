adce2bf0a8c467b21ca4edcffc85857e56e70e27
preservance717 <GL717@outlook.com>
2016-07-25 18:47:33 +0800

    modify format

--- src/fixtures.js
+++ src/fixtures.js
@@ -7,7 +7,7 @@ function loadAllItems() {
     new Items('ITEM000002', '苹果', '斤', 5.50),
     new Items('ITEM000003', '荔枝', '斤', 15.00),
     new Items('ITEM000004', '电池', '个', 2.00),
-    new Items('ITEM000005', '方便面', '袋', 4.50),
+    new Items('ITEM000005', '方便面', '袋', 4.50)
   ];
 }
 
---
+++ src/models/promotions.js
@@ -0,0 +1,3 @@
+/**
+ * Created by gaole on 7/25/16.
+ */
