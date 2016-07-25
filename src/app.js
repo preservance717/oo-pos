130f5f343d037774b074c8ed498fc189103e58d6
preservance717 <GL717@outlook.com>
2016-07-25 22:12:00 +0800

    move buildReceiptText to Receipt.buildReceiptText

--- src/app.js
+++ src/app.js
@@ -7,46 +7,10 @@ const Promotions = require('./models/promotions');
 function printReceipt(tags) {
   const cartItems = CartItems.buildCartItems(tags, Items.all());
   const receiptItems = ReceiptItems.buildReceiptItems(cartItems, Promotions.all());
-  const receipt = buildReceipt(receiptItems);
-  const receiptText = buildReceiptText(receipt);
+  const receipt = new Receipt(receiptItems);
+  const receiptText = Receipt.buildReceiptText(receipt);
 
   console.log(receiptText);
-}
-
-function buildReceipt(receiptItems) {
-  let total = 0;
-  let savedTotal = 0;
-
-  for (const receiptItem of receiptItems) {
-    total += receiptItem.subtotal;
-    savedTotal += receiptItem.saved;
-  }
-
-  return new Receipt(receiptItems, total, savedTotal);
-}
-
-function buildReceiptText(receipt) {
-
-  let receiptItemsText = receipt.receiptItems
-    .map(receiptItem => {
-      const cartItem = receiptItem.cartItem;
-      return `名称：${cartItem.item.name}，\
-数量：${cartItem.count}${cartItem.item.unit}，\
-单价：${formatMoney(cartItem.item.price)}(元)，\
-小计：${formatMoney(receiptItem.subtotal)}(元)`;
-    })
-    .join('\n');
-
-  return `***<没钱赚商店>收据***
-${receiptItemsText}
-----------------------
-总计：${formatMoney(receipt.total)}(元)
-节省：${formatMoney(receipt.savedTotal)}(元)
-**********************`;
-}
-
-function formatMoney(money) {
-  return money.toFixed(2);
 }
 
 exports.printReceipt = printReceipt;
--- src/models/receipt.js
+++ src/models/receipt.js
@@ -1,8 +1,39 @@
-class Receipt{
-  constructor(receiptItems, total = 0, savedTotal = 0){
+class Receipt {
+  constructor(receiptItems) {
+    let total = 0;
+    let savedTotal = 0;
+
+    for (const receiptItem of receiptItems) {
+      total += receiptItem.subtotal;
+      savedTotal += receiptItem.saved;
+    }
+
     this.receiptItems = receiptItems;
     this.total = total;
     this.savedTotal = savedTotal;
+  }
+
+  static buildReceiptText(receipt) {
+    const formatMoney = (money) => {
+      return money.toFixed(2);
+    };
+     
+    let receiptItemsText = receipt.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(receipt.total)}(元)
+节省：${formatMoney(receipt.savedTotal)}(元)
+**********************`;
   }
 }
 
