5a18ff700b6639a2494e4b44d5554a9f83d451ac
preservance717 <GL717@outlook.com>
2016-07-25 18:41:02 +0800

    create Items class

---
+++ src/models/items.js
@@ -0,0 +1,10 @@
+class Items{
+  constructor(barcode, name, unit, price = 0){
+    this.barcode = barcode;
+    this.name = name;
+    this.unit = unit;
+    this.price = price;
+  }
+}
+
+module.exports = Items;
